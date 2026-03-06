b8110e3b7bb9e75d3e6d429a1ba23405b0f7d40e
Jeremie Christopher <Jeremiechristopher11@gmail.com>
2026-03-05 22:32:04 -0500

    Update embed sources with more reliable providers

--- src/api/tmdb.js
+++ src/api/tmdb.js
@@ -19,29 +19,44 @@ export const tmdb = async (endpoint, params = {}) => {
 // Embed sources - multiple providers for better coverage
 export const EMBED_SOURCES = [
   {
-    name: 'VidSrc',
-    movie: (id) => `https://vidsrc.xyz/embed/movie/${id}`,
-    tv: (id, s, e) => `https://vidsrc.xyz/embed/tv/${id}/${s}/${e}`
+    name: 'VidSrc Pro',
+    movie: (id) => `https://vidsrc.pro/embed/movie/${id}`,
+    tv: (id, s, e) => `https://vidsrc.pro/embed/tv/${id}/${s}/${e}`
   },
   {
-    name: 'VidSrc.to',
-    movie: (id) => `https://vidsrc.to/embed/movie/${id}`,
-    tv: (id, s, e) => `https://vidsrc.to/embed/tv/${id}/${s}/${e}`
+    name: 'VidSrc.cc',
+    movie: (id) => `https://vidsrc.cc/v2/embed/movie/${id}`,
+    tv: (id, s, e) => `https://vidsrc.cc/v2/embed/tv/${id}/${s}/${e}`
   },
   {
-    name: '2Embed',
-    movie: (id) => `https://www.2embed.cc/embed/${id}`,
-    tv: (id, s, e) => `https://www.2embed.cc/embedtv/${id}&s=${s}&e=${e}`
+    name: 'VidSrc.icu',
+    movie: (id) => `https://vidsrc.icu/embed/movie/${id}`,
+    tv: (id, s, e) => `https://vidsrc.icu/embed/tv/${id}/${s}/${e}`
   },
   {
-    name: 'SuperEmbed',
-    movie: (id) => `https://multiembed.mov/?video_id=${id}&tmdb=1`,
-    tv: (id, s, e) => `https://multiembed.mov/?video_id=${id}&tmdb=1&s=${s}&e=${e}`
+    name: 'Embed.su',
+    movie: (id) => `https://embed.su/embed/movie/${id}`,
+    tv: (id, s, e) => `https://embed.su/embed/tv/${id}/${s}/${e}`
   },
   {
-    name: 'VidLink',
-    movie: (id) => `https://vidlink.pro/movie/${id}`,
-    tv: (id, s, e) => `https://vidlink.pro/tv/${id}/${s}/${e}`
+    name: 'AutoEmbed',
+    movie: (id) => `https://autoembed.co/movie/tmdb/${id}`,
+    tv: (id, s, e) => `https://autoembed.co/tv/tmdb/${id}-${s}-${e}`
+  },
+  {
+    name: 'SmashyStream',
+    movie: (id) => `https://player.smashy.stream/movie/${id}`,
+    tv: (id, s, e) => `https://player.smashy.stream/tv/${id}?s=${s}&e=${e}`
+  },
+  {
+    name: 'NontonGo',
+    movie: (id) => `https://www.NontonGo.win/embed/movie/${id}`,
+    tv: (id, s, e) => `https://www.NontonGo.win/embed/tv/${id}/${s}/${e}`
+  },
+  {
+    name: 'MoviesAPI',
+    movie: (id) => `https://moviesapi.club/movie/${id}`,
+    tv: (id, s, e) => `https://moviesapi.club/tv/${id}-${s}-${e}`
   }
 ];
 
